6420c1c7e0c912c120389d129d29783175accb22
nitis <nitish8152@gmail.com>
2022-11-18 16:01:55 +0530

    Cab Invoice Generator

---
+++ src/com/bridgelab/Rides.java
@@ -0,0 +1,13 @@
+package com.bridgelab;
+
+public class Rides {
+    int distance;
+    int duration;
+    enum RideType{
+        NORMAL,PREMIUM
+    };
+    public Rides(int distance, int duration) {
+        this.distance = distance;
+        this.duration = duration;
+    }
+}
